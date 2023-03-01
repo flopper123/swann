ac962c673f9571f1f0fda10810e5d04c1563df4c
Thomas <thomas.schauser@gmail.com>
2023-03-01 15:56:12 +0100

    Added benchmarks of loading

--- src/benchmark/loaddataset_benchmark.cc
+++ src/benchmark/loaddataset_benchmark.cc
@@ -1,20 +1,36 @@
-// #include <benchmark/benchmark.h>
+#include <benchmark/benchmark.h>
 
-// #include "/swann/dataset/load.hpp"
+#include "../dataset/load.hpp"
 
-// static void DoSetup(const benchmark::State &state)
-// {
-//   Dataset in;
-//   load_hdf5(in, DataSize::XS);
-// }
+static void DoSetup(const benchmark::State &state)
+{
+  Dataset out = load_hdf5(DataSize::XS);
+}
 
-// void BM_load_XS_dataset(benchmark::State &state)
-// {
-//   for (auto _ : state) {
-//     // Dataset in;
-//     // load_hdf5(in, DataSize::XS);
-//     int i = 4;
-//   }
-// }
-// // Register the function as a benchmark
-// BENCHMARK(BM_load_XS_dataset)->Setup(DoSetup);
+static void BM_load_XS_dataset(benchmark::State &state)
+{
+  for (auto _ : state) {
+    Dataset out = load_hdf5(DataSize::XS);
+  }
+}
+BENCHMARK(BM_load_XS_dataset)->Setup(DoSetup);
+
+static void BM_parse_XS_dataset(benchmark::State &state)
+{
+  // Setup
+  DataSize size = DataSize::XS;
+  H5::DataSet dataset = fetch_dataset(size);
+
+  auto [rows, cols] = get_dataset_dimensions(dataset);
+
+  std::vector<ui64> data_output(rows * cols);
+
+  dataset.read(&data_output[0], H5::PredType::NATIVE_UINT64);
+
+  // Time
+  for (auto _ : state) {
+    Dataset out = parse_dataset(data_output, rows, cols);
+  }
+}
+
+BENCHMARK(BM_parse_XS_dataset)->Setup(DoSetup);
