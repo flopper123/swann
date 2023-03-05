d7df2bab569587e97530b620487ccacd06bf0b54
Christoffer Woldbye <woldby3@hotmail.com>
2023-03-05 23:48:43 +0100

    improved prints from main

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,16 @@ HashPool<D> hashes;
 
 int main()
 {
-  std::cout << "Loading Dataset..." << std::endl;
+  std::cout << "[+] Loading Dataset..." << std::endl;
   Dataset in = load_hdf5(DataSize::XS);
-  std::cout << "Loading Index..." << std::endl;
+  std::cout << "[+] Loading Index..." << std::endl;
   auto maps = LSHMapFactory<D>::create(hashes, TRIE_DEPTH, TRIE_COUNT);
   LSHForest<D> index(maps, in);
-  std::cout << "Building Index..." << std::endl;
+  std::cout << "[+] Building Index..." << std::endl;
   index.build();
-  std::cout << "Index build on:"
+  std::cout << "[+] Index build on:"
             << "\n\t" << index.size() << " points  "
             << "\n\t" << index.getMaps().size() << " tries" << std::endl
-            << "ready for querying <3" << std::endl;
+            << "[+] ready for querying <3" << std::endl;
   return 0;
 }
